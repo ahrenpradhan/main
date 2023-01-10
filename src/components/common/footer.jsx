b24d2874d57f70a47833b2eb5b3fa166ff4ebf0e
ahrenpradhan <ahrenpradhan@gmail.com>
2023-01-10 18:20:27 +0530

    add footer

---
+++ src/components/common/footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => (
+  <div className="border border-zinc-400 py-4 text-center">
+    <div>And yeah, thats it !!!</div>
+    <div>@ 2023 Ahren Pradhan.</div>
+  </div>
+);
+
+export default Footer;
