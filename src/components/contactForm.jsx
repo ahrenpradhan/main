4ed92262423ec55f6d63031630ae1f8ca5777ec0
ahrenpradhan <ahrenpradhan@gmail.com>
2023-02-19 02:52:14 +0530

    fix contactform send btn bg color and overlap with navbar

--- src/components/contactForm.jsx
+++ src/components/contactForm.jsx
@@ -43,7 +43,7 @@ const ContactForm = () => {
           type="text"
           onChange={e => handleChange('firstname', e.target.value)}
           required
-          className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+          className="block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
           placeholder="First Name *"
         />
         <input
@@ -52,7 +52,7 @@ const ContactForm = () => {
           onChange={e => handleChange('lastname', e.target.value)}
           type="text"
           required
-          className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+          className="block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
           placeholder="Last Name *"
         />
         <input
@@ -62,7 +62,7 @@ const ContactForm = () => {
           type="email"
           autoComplete="email"
           required
-          className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+          className="block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
           placeholder="Email address *"
         />
         <input
@@ -70,7 +70,7 @@ const ContactForm = () => {
           name="company"
           onChange={e => handleChange('company', e.target.value)}
           type="text"
-          className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+          className="block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
           placeholder="Company"
         />
         <textarea
@@ -80,7 +80,7 @@ const ContactForm = () => {
           onChange={e => handleChange('note', e.target.value)}
           type="text"
           required
-          className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+          className="block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
           placeholder="Note *"
         />
       </div>
@@ -88,7 +88,7 @@ const ContactForm = () => {
         disabled={!enabled}
         type="reset"
         onClick={handleSend}
-        className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        className="group flex w-full justify-center rounded-md border border-transparent bg-zinc-700 py-2 px-4 text-sm font-medium text-white hover:bg-zinc-900 focus:outline-none cursor-pointer focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
       >
         Send
       </button>
