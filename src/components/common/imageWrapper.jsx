0b12581133a2ddf20302c27eb6bcc80f5bae9794
ahrenpradhan <ahrenpradhan@gmail.com>
2023-02-18 18:00:04 +0530

    update worked content

--- src/components/common/imageWrapper.jsx
+++ src/components/common/imageWrapper.jsx
@@ -1,17 +1,23 @@
-const ImageWrapper = ({ svg, children, alt, className }) => (
+const ImageWrapper = ({ svg, src, children, alt, className, imgStyle }) => (
   <div
     className={`mb-4 w-2/5 max-w-full self-center md:max-w-fit ${
       className || ''
     }`}
   >
-    {svg ? <img className="mx-auto" src={svg} alt={alt} /> : children}
+    {svg || src ? (
+      <img className="mx-auto" src={svg || src} alt={alt} style={imgStyle} />
+    ) : (
+      children
+    )}
   </div>
 );
 
 ImageWrapper.defaultProps = {
   svg: false,
+  src: false,
   alt: 'random',
-  className: false
+  className: false,
+  imgStyle: {}
 };
 
 export default ImageWrapper;
--- src/views/home/worked.jsx
+++ src/views/home/worked.jsx
@@ -1,5 +1,10 @@
+import AlgoliaLogo from '@/Assets/companies/algolia.svg';
+import AyasyaLogo from '@/Assets/companies/ayasya.png';
+import CommerceToolsLogo from '@/Assets/companies/commerceTools.svg';
 import MmjsLogo from '@/Assets/companies/mmjs.png';
 import NEOsoftLogo from '@/Assets/companies/neosoft.svg';
+import ParagyteLogo from '@/Assets/companies/paragyte.png';
+import TagtasteLogo from '@/Assets/companies/tagtaste.svg';
 import TeknofeetLogo from '@/Assets/companies/teknofeet.svg';
 import { Divider, ImageWrapper } from '@/Components/common/index';
 // section 3 - Worked
@@ -12,16 +17,39 @@ const Worked = () => (
     <div>
       <Divider />
       <div className="min-w-full pb-8 text-center text-3xl font-bold tracking-wide">
-        Worked with
+        <span>Freelanced With | </span>
+        <span>Employeed At | </span>
+        <span>Outsourced To</span>
       </div>
       <div className="flex flex-wrap justify-center gap-4 md:gap-8">
-        <ImageWrapper svg={NEOsoftLogo} alt="teknofeet logo" />
-        <ImageWrapper svg={MmjsLogo} alt="teknofeet logo" />
+        <ImageWrapper
+          src={AlgoliaLogo}
+          alt="algolia logo"
+          imgStyle={{ width: '16em' }}
+        />
+        <ImageWrapper
+          src={CommerceToolsLogo}
+          alt="commercetools logo"
+          imgStyle={{ width: '16em' }}
+        />
+        <ImageWrapper
+          src={AyasyaLogo}
+          alt="ayasya logo"
+          imgStyle={{ maxWidth: '16em' }}
+        />
+        <ImageWrapper
+          svg={TagtasteLogo}
+          alt="tagtaste logo"
+          imgStyle={{ width: '16em' }}
+        />
+        <ImageWrapper svg={NEOsoftLogo} alt="neosoft logo" />
+        <ImageWrapper src={MmjsLogo} alt="mmjs logo" />
         <ImageWrapper
           svg={TeknofeetLogo}
           className="w-3/4 md:w-1/2 md:max-w-none"
           alt="teknofeet logo"
         />
+        <ImageWrapper src={ParagyteLogo} className="bg-black p-4" alt="mmjs logo" />
       </div>
     </div>
   </section>
